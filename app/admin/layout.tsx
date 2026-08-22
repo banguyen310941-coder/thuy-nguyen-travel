b121e7aa76a79db54256aae69dd23c4924c0b42d
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-22 22:35:04 +0700

    Load specialized product editor styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,5 +2,6 @@ import './admin.css';
 import './bookings.css';
 import './cms.css';
 import './editor.css';
+import './product-v2.css';
 
 export default function AdminLayout({children}:{children:React.ReactNode}){return <>{children}</>}
