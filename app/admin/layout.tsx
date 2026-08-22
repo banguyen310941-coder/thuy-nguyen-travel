ee64e07dcd30ce4d6051e41cd02a7b4c3054ace2
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-22 22:46:19 +0700

    Load unit inventory editor styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -4,5 +4,6 @@ import './cms.css';
 import './editor.css';
 import './product-v2.css';
 import './product-manager.css';
+import './units.css';
 
 export default function AdminLayout({children}:{children:React.ReactNode}){return <>{children}</>}
