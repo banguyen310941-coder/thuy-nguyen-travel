0db8332897a6ab4850cc49da7815f5d3fa1305c8
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-22 22:40:27 +0700

    Load product manager styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,5 +3,6 @@ import './bookings.css';
 import './cms.css';
 import './editor.css';
 import './product-v2.css';
+import './product-manager.css';
 
 export default function AdminLayout({children}:{children:React.ReactNode}){return <>{children}</>}
