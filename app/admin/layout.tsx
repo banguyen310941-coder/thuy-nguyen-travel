c01a06d0bea254798a2912b4b6e32ed4210b7ae8
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-22 22:20:03 +0700

    Load article and product editor styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,5 +1,6 @@
 import './admin.css';
 import './bookings.css';
 import './cms.css';
+import './editor.css';
 
 export default function AdminLayout({children}:{children:React.ReactNode}){return <>{children}</>}
