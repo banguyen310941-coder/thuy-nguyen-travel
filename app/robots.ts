715ae448fcbbec0d138eb8aa150d4bc69f6aacf5
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-23 06:40:30 +0700

    Block admin correctly on GitHub Pages and future hosting

--- app/robots.ts
+++ app/robots.ts
@@ -4,7 +4,11 @@ export const dynamic = 'force-static';
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [{ userAgent: '*', allow: '/', disallow: ['/admin/'] }],
+    rules: [{
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/thuy-nguyen-travel/admin/'],
+    }],
     sitemap: 'https://banguyen310941-coder.github.io/thuy-nguyen-travel/sitemap.xml',
   };
 }
