fb1cf15280d22142a9ca08c5569bcabc05d90af8
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-22 23:02:39 +0700

    Load improved admin editor styles

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,7 @@ import './admin.css';
 import './bookings.css';
 import './cms.css';
 import './editor.css';
+import './editor-pro-v2.css';
 import './product-v2.css';
 import './product-manager.css';
 import './units.css';
