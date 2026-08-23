e3394e66c16c65b4be306397dfa0631d5e610f45
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-23 08:51:28 +0700

    Tighten robots rules for non-indexable CMS and search routes

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,18 @@ export default function robots(): MetadataRoute.Robots {
     rules: [{
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin/', '/thuy-nguyen-travel/admin/'],
+      disallow: [
+        '/admin/',
+        '/thuy-nguyen-travel/admin/',
+        '/search',
+        '/thuy-nguyen-travel/search',
+        '/product',
+        '/thuy-nguyen-travel/product',
+        '/tour-product',
+        '/thuy-nguyen-travel/tour-product',
+        '/guide/read',
+        '/thuy-nguyen-travel/guide/read',
+      ],
     }],
     sitemap: 'https://banguyen310941-coder.github.io/thuy-nguyen-travel/sitemap.xml',
   };
