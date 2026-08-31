25cf7870a5d59ea072a8222cb704ec198d6ab982
banguyen310941-coder <banguyen310941@gmail.com>
2026-08-31 14:38:49 +0700

    Add HappyGo contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,1 @@
+import {HappyGoInfoPage} from '@/components/HappyGoInfoPage';export const metadata={title:'Liên hệ HappyGo Travel'};export default function Page(){return <HappyGoInfoPage kind="contact"/>}
